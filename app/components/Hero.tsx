a04c6a557ef7866d2c9c1238dd34f30b5d420fbf
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-15 01:35:02 +0300

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="grid grid-cols-2 max-lg:grid-cols-1 max-lg:gap-10 w-full lg:h-4/6 items-center">
       <div className="flex flex-col max-lg:w-10/12 max-md:w-full max-lg:mx-auto gap-10 mt-10 font-medium">
-        <h1 className="text-slate-700 font-bold text-5xl max-md:text-4xl">
+        <h1 className="text-themeText-100 font-bold text-5xl max-md:text-4xl">
           Try Salesforce Starter Suite for free.
         </h1>
         <p className="text-lg">
