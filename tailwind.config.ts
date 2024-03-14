c3376ae3ff2edf235fafe9b3df4ecf2bdb8f47dd
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-15 01:46:59 +0300

    Theme Colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        themeText:{
+          100:"rgb(3, 45, 96)",
+          200:"rgb(1, 118, 211)",
+          300:"rgb(11, 92, 171)"
+        },
+        primary:{
+          100:"#0284c7",
+          200:"#e0f2fe",
+        },
+        secondary:{
+          100:"#047857",
+          200:"#065f46",
+        }
+      }
     },
   },
   plugins: [],
