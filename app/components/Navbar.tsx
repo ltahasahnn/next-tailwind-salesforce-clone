1a5a4c492a8880466cb03b852f57625d4ad65b01
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-15 01:35:08 +0300

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -39,11 +39,11 @@ const Navbars = () => {
               </a>
             </div>
             <div className="nav-items max-xl:hidden">
-              <ul className="nav-item-list flex items-center text-primary-100 font-bold gap-5">
+              <ul className="nav-item-list flex items-center text-themeText-100 font-bold gap-5">
                 {navItemList.map((item, index) => (
                   <li
                     key={index}
-                    className="nav-item hover:text-sky-600 duration-200 cursor-pointer min-w-16"
+                    className="nav-item hover:text-primary-100 duration-200 cursor-pointer min-w-16"
                   >
                     {item.title}
                   </li>
@@ -53,26 +53,26 @@ const Navbars = () => {
             <div className="utility-nav ml-auto flex items-center">
               <div className="utility icons flex items-center gap-4">
                 <div className="contact flex flex-col font-light text-center max-xl:hidden">
-                  <span className="underline hover:text-sky-600 duration-200 cursor-pointer">
+                  <span className="underline hover:text-primary-100 duration-200 cursor-pointer">
                     Contact Us
                   </span>
                   <span>00800 7253 3333</span>
                 </div>
 
-                <div className="search-icon xl:hover:bg-sky-100 xl:hover:text-sky-600 duration-100 cursor-pointer p-3 rounded-full">
+                <div className="search-icon xl:hover:bg-primary-200 xl:hover:text-primary-100 duration-100 cursor-pointer p-3 rounded-full">
                   <FaSearch />
                 </div>
-                <div className="globe-icon xl:hover:bg-sky-100 xl:hover:text-sky-600 duration-100 cursor-pointer p-3 rounded-full max-xl:hidden">
+                <div className="globe-icon xl:hover:bg-primary-200 xl:hover:text-primary-100 duration-100 cursor-pointer p-3 rounded-full max-xl:hidden">
                   <FaGlobe />
                 </div>
-                <div className="user-icon xl:hover:bg-sky-100 xl:hover:text-sky-600 duration-100 cursor-pointer p-3 rounded-full flex items-center gap-2 font-bold text-sm">
+                <div className="user-icon xl:hover:bg-primary-200 xl:hover:text-primary-100 duration-100 cursor-pointer p-3 rounded-full flex items-center gap-2 font-bold text-sm">
                   <FaUser />
                   <span className="max-xl:hidden">Login</span>
                 </div>
               </div>
               <div className="ml-4">
                 <Button
-                  className="bg-emerald-700 border-emerald-700 hover:bg-emerald-800 hover:border-emerald-800 w-32 py-1.5"
+                  className="bg-secondary-100 border-secondary-100 hover:bg-secondary-200 hover:border-secondary-200 !w-32 text-base !px-2 !py-2"
                   value="Try for Sales"
                 />
               </div>
