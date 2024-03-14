e902fad595f46425d395c6d221b5b6d917a710c2
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-14 16:14:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import Navbars from "./components/Navbar";
+import Hero from "./components/Hero";
+import News from "./components/News";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex h-dvh overflow-hidden flex-col">
+      <Navbars />
+      <div className="xl:container overflow-scroll mx-auto h-dvh max-xl:px-6">
+        <Hero />
+        <News />
+      </div>
+    </main>
   );
 }
