f7aebde7da5e6be06260f76cae6606e4d88174cd
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-15 01:35:33 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Image from "next/image";
 import Navbars from "./components/Navbar";
 import Hero from "./components/Hero";
 import News from "./components/News";
+import Demos from "./components/Demos";
 
 export default function Home() {
   return (
     <main className="flex h-dvh overflow-hidden flex-col">
       <Navbars />
-      <div className="xl:container overflow-scroll mx-auto h-dvh max-xl:px-6">
-        <Hero />
-        <News />
+      <div className="w-full h-dvh overflow-scroll">
+        <div className="xl:container mx-auto h-full max-xl:px-6">
+          <Hero />
+          <News />
+          <Demos />
+        </div>
       </div>
     </main>
   );
