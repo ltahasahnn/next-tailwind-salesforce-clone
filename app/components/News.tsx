5ba1fb1df57ba7d65cd0fc22a611a6ba877c7e3e
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-15 00:35:14 +0300

    Update News.tsx

--- app/components/News.tsx
+++ app/components/News.tsx
@@ -18,24 +18,31 @@ const News = () => {
   ];
 
   return (
-    <div className="my-12 ">
-      <h2 className="text-center text-4xl mb-20 font-bold text-slate-800">
+    <div className="my-12 h-fit">
+      <h2 className="text-center max-md:text-3xl text-4xl max-md:mb-6 md:mb-20 font-semibold text-primary-100">
         What's new at Salesforce?
       </h2>
 
-      <div className="grid grid-cols-3 gap-12">
+      <div className="grid grid-cols-3 max-md:!grid-cols-1 shrink-0 h-fit gap-10 max-xl:gap-5 max-lg:gap-3">
         {list.map((item, index) => (
-          <div key={index} className="flex flex-col rounded-xl">
-            <div className="h-60">
+          <div
+            key={index}
+            className="w-full cursor-pointer shadow-[0_2px_10px_0px_rgba(0,0,0,.3)] rounded-xl"
+          >
+            <div className="max-lg max-lg:h-44 max-md:h-fit">
               <img
-                className="w-full rounded-t-xl h-full object-cover"
+                className="w-full rounded-t-xl h-full square-radio object-cover"
                 src={item.image}
                 alt=""
               />
             </div>
-            <div className="h-60 flex flex-col justify-between py-16 px-8">
-              <span>{item.text}</span>
-              <span>{item.link}</span>
+            <div className="flex flex-col aspect-auto justify-between h-60 max-md:h-40 max-md:py-8 max-lg:h-44 max-lg:space-y-0 max-lg:pt-6 max-lg:p-6 space-y-4 p-8 font-semibold text-slate-700">
+              <span className="text-2xl max-lg:text-lg max-md:text-xl">
+                {item.text}
+              </span>
+              <span className="duration-300 hover:text-blue-600 hover:no-underline underline">
+                {item.link}
+              </span>
             </div>
           </div>
         ))}
