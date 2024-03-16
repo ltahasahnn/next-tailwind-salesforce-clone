5659159b71cb36ae2c79b011b0671701d25dcfe7
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 13:52:44 +0300

    Update Hero.tsx

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,17 +2,17 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <div className="grid montserrat grid-cols-2 max-lg:grid-cols-1 max-lg:gap-10 w-full lg:h-4/6 items-center">
-      <div className="flex flex-col max-lg:w-10/12 max-md:w-full max-lg:mx-auto gap-10 mt-10 font-medium">
-        <h1 className="text-themeText-100 max-sm:w-full leading-[3.5rem] font-extrabold text-5xl w-8/12 max-md:text-4xl">
+    <div className="grid montserrat grid-cols-2 w-full lg:h-4/6 items-center max-lg:gap-10 max-lg:grid-cols-1 mx-auto max-lg:max-w-2xl">
+      <div className="flex flex-col gap-10 mt-10 font-medium">
+        <h1 className="text-themeText-100 max-sm:w-full font-black text-5xl max-xl:max-w-lg max-lg:max-w-full max-md:text-4xl">
           Try Salesforce Starter Suite for free.
         </h1>
-        <p className="text-xl font-light max">
+        <p className="text-xl font-light xl:mr-auto xl:max-w-lg max-xl:max-w-md max-lg:max-w-full">
           Unite sales, service and email outreach in a single app. Try
           Salesforce Starter Suite today.
         </p>
 
-        <div className="flex gap-2 max-md:flex-col">
+        <div className="flex gap-2 max-md:flex-col max-lg:gap-6">
           <Button value="Start free trial" className="max-md:w-full" />
           <Button outline value="Watch Demos" className="max-md:w-full" />
         </div>
