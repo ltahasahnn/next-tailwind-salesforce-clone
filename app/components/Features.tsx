57af226c0a465e78608504b713052b95a7841352
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 14:33:24 +0300

    Update Features.tsx

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -27,7 +27,7 @@ const Features = () => {
             Find innovations for any industry.
           </h2>
 
-          <div className="text-slate-600 font-light mb-12 mt-8 mx-auto text-lg md:max-lg:max-w-xl max-xl:!text-md max-xl:mb-10 max-xl:max-w-3xl">
+          <div className="text-slate-600 font-light mb-12 mt-8 mx-auto text-lg md:max-lg:max-w-xl max-md:mt-4 max-xl:!text-md max-xl:mb-10 max-xl:max-w-3xl">
             <p>
               Whatever your industry, we offer solutions to modernise your
               business, save time, and lower costs.
@@ -43,11 +43,11 @@ const Features = () => {
 
         <div className="grid grid-cols-4 mx-auto gap-4 max-sm:grid-cols-2 max-sm:gap-y-6 max-lg:gap-0">
           {list.map((item, index) => (
-            <div className="w-36">
+            <div className="w-32">
               <div className="rounded-full mb-4 w-24 h-24 p-6 mx-auto shadow-[0_5px_10px_0px_rgba(0,0,0,.2)] max-lg:h-20 max-lg:w-20">
                 <img src={item.avatar} alt="" />
               </div>
-              <div className="text-center font-semibold text-base mx-auto px-1">
+              <div className="text-center font-bold text-base mx-auto">
                 <a href="#">{item.text}</a>
               </div>
             </div>
