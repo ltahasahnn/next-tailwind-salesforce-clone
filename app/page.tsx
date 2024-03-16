e71d470389fd788838410c8c232f764bb22b1f0a
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 13:03:05 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
-import Image from "next/image";
 import Navbars from "./components/Navbar";
 import Hero from "./components/Hero";
 import News from "./components/News";
 import Demos from "./components/Demos";
+import Features from "./components/Features";
+import Customers from "./components/Customers";
 
 export default function Home() {
   return (
     <main className="flex h-dvh overflow-hidden flex-col">
       <Navbars />
       <div className="w-full h-dvh overflow-scroll">
-        <div className="xl:container mx-auto h-full max-xl:px-6">
-          <Hero />
-          <News />
-          <Demos />
+        <div>
+          <div className="xl:container mx-auto h-full max-xl:px-6">
+            <Hero />
+            <News />
+            <Demos />
+            <Features />
+          </div>
+          <div>
+            <img src="../assets/images/rounded.png" alt="" />
+            <div className="max-xl:px-6">
+              <Customers />
+            </div>
+            <img src="../assets/images/hp-page-break-img-hills.webp" alt="" />
+          </div>
         </div>
       </div>
     </main>
