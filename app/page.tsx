765afe2dfa629b740e197614f43f5b2c6a14f90d
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 16:14:55 +0300

    Update Image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Demos from "./components/Demos";
 import Features from "./components/Features";
 import Customers from "./components/Customers";
 import Learning from "./components/Learning";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -19,12 +20,12 @@ export default function Home() {
             <Features />
           </div>
           <div>
-            <img src="../assets/images/rounded.png" alt="" />
+            <Image src="../assets/images/rounded.png" alt="" />
             <div className="max-xl:px-6">
               <Customers />
               <Learning />
             </div>
-            <img src="../assets/images/hp-page-break-img-hills.webp" alt="" />
+            <Image src="../assets/images/hp-page-break-img-hills.webp" alt="" />
           </div>
         </div>
       </div>
