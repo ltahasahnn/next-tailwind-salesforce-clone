15e30a46fefc6baea4e0f67dd3e27f079d0973b9
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 13:25:08 +0300

    Update Customers.tsx

--- app/components/Customers.tsx
+++ app/components/Customers.tsx
@@ -21,13 +21,13 @@ const Customers = () => {
 
   return (
     <div className="flex flex-col text-center gap-16 text-themeText-100">
-      <div className="w-7/12 max-lg:w-10/12 max-md:w-full mx-auto">
-        <h2 className="font-semibold xl:text-4xl max-xl:text-3xl mx-auto leading-[3rem] tracking-[-0.32px] xl:montserrat max-lg:px-10 max-md:px-0">
+      <div className="max-w-3xl max-[1160px]:max-w-xl max-md:w-full mx-auto">
+        <h2 className="!font-black xl:text-4xl max-xl:text-3xl mx-auto leading-10 tracking-[-0.32px] montserrat">
           See how companies drive customer success in a whole new way with
           Einstein 1.
         </h2>
         <div>
-          <p className="text-base mt-8 mb-10 xl:px-10 max-md:px-0 max-lg:px-20">
+          <p className="text-base mt-8 mb-10">
             Learn how companies of every description are using CRM, trusted AI,
             and data to be more productive and grow customer loyalty.
           </p>
@@ -38,10 +38,10 @@ const Customers = () => {
           className="border-primary-100 text-primary-100 w-60 py-3 max-md:w-full rounded-[.35rem]"
         />
       </div>
-      <div className="flex max-lg:flex-wrap justify-center gap-4 w-11/12 max-md:w-full mx-auto">
+      <div className="flex max-[1160px]:flex-wrap justify-center gap-4 max-[1160px]:!max-w-3xl max-w-7xl max-md:w-full mx-auto">
         {list.map((item, index) => (
           <div
-            className="shadow-[0_3px_8px_0_rgba(0,0,0,.15)] w-full flex h-24 items-center justify-center rounded-lg sm:max-w-96 max-sm:w-36 max-lg:w-3/12 max-md:h-20"
+            className="shadow-[0_3px_8px_0_rgba(0,0,0,.15)] w-full flex h-24 items-center justify-center rounded-lg min-w-48 max-[1160px]:max-w-52"
             key={index}
           >
             <img
