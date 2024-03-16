9c4154a688e872b3a2e69a380ff2948a7bae6054
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-16 14:15:19 +0300

    Update Features.tsx

--- app/components/Features.tsx
+++ app/components/Features.tsx
@@ -20,15 +20,15 @@ const Features = () => {
     },
   ];
   return (
-    <div className="flex py-20 items-center text-themeText-100 justify-center text-center">
-      <div className="flex flex-col gap-16 max-sm:gap-12">
-        <div>
+    <div className="flex py-20 items-center w-full text-themeText-100 justify-center text-center">
+      <div className="flex flex-col gap-16 max-sm:gap-12 w-full">
+        <div className="w-full">
           <h2 className="text-4xl font-bold montserrat max-xl:text-3xl">
             Find innovations for any industry.
           </h2>
 
-          <div className="mt-8 max-xl:mb-10 mb-12 max-xl:w-9/12 max-lg:w-8/12 max-md:w-full mx-auto text-lg max-xl:!text-md text-slate-600">
-            <p className="font-light">
+          <div className="text-slate-600 font-light mb-12 mt-8 mx-auto text-lg md:max-lg:max-w-xl max-xl:!text-md max-xl:mb-10 max-xl:max-w-3xl">
+            <p>
               Whatever your industry, we offer solutions to modernise your
               business, save time, and lower costs.
             </p>
@@ -37,14 +37,14 @@ const Features = () => {
           <Button
             value="See all industries"
             outline
-            className="border-primary-100 text-primary-100 w-48 py-3 max-md:w-full rounded-[.35rem]"
+            className="border-primary-100 text-primary-100 w-48 py-3 max-md:w-full rounded-md"
           />
         </div>
 
-        <div className="grid grid-cols-4 mx-auto gap-4 w-fit max-sm:grid-cols-2 max-sm:gap-y-6 max-lg:gap-0">
+        <div className="grid grid-cols-4 mx-auto gap-4 max-sm:grid-cols-2 max-sm:gap-y-6 max-lg:gap-0">
           {list.map((item, index) => (
             <div className="w-36">
-              <div className="rounded-full mx-auto shadow-[0_5px_10px_0px_rgba(0,0,0,.2)] p-5 mb-4 w-[5.5rem] h-[5.5rem]">
+              <div className="rounded-full mb-4 w-24 h-24 p-6 mx-auto shadow-[0_5px_10px_0px_rgba(0,0,0,.2)] max-lg:h-20 max-lg:w-20">
                 <img src={item.avatar} alt="" />
               </div>
               <div className="text-center font-semibold text-base mx-auto px-1">
