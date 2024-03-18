1759e209e9e1dd3307f65a31c926875c17754cbb
qirmizisortl1 <119079410+qirmizisortl1@users.noreply.github.com>
2024-03-18 09:51:27 +0300

    Add Hero Video

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,8 +1,17 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
 import Button from "./Button";
 import HeroImage from "../../public/assets/images/hero-image.png";
+import Image from "next/image";
 
 const Hero = () => {
+  const [showVideo, setShowVideo] = useState(false);
+
+  const toggleVideo = () => {
+    setShowVideo(!showVideo);
+  };
+
   return (
     <div className="grid NotoSans grid-cols-2 w-full max-w-7xl lg:h-4/6 items-center max-lg:gap-10 max-lg:grid-cols-1 mx-auto max-lg:max-w-2xl">
       <div className="flex flex-col gap-10 mt-10 font-medium">
@@ -20,13 +29,21 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="h-full">
-        <Image
-          draggable="false"
-          className="h-full w-full object-contain"
-          src={HeroImage}
-          alt=""
-        />
+      <div
+        className="h-full relative mt-8 min-h-72 cursor-pointer max-sm:min-h-60"
+        onClick={toggleVideo}
+      >
+        {showVideo ? (
+          <video controls autoPlay className="object-cover h-full rounded-2xl">
+            <source src="assets/videos/erdogan.mp4" />
+          </video>
+        ) : (
+          <Image
+            src={HeroImage}
+            alt="Hero Image"
+            className="object-cover w-full h-full absolute rounded-2xl top-0 left-0"
+          />
+        )}
       </div>
     </div>
   );
